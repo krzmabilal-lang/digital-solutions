46e05fced0d544fc04a10fd4a4f74a0edef7ab0d
krzmabilal-lang <krzmabilal@gmail.com>
2026-07-08 15:15:11 +0100

    Delete lib/constants.ts

--- lib/constants.ts
+++
@@ -1,3 +0,0 @@
-export const SITE_URL = 'https://digital-solutions-6mi4.vercel.app';
-export const SITE_NAME = 'Digital Solutions';
-export const AFFILIATE_LINK = 'https://whop.com/toolsuite/buy-vip?a=httpsdigital-solutions-6mi4vercelapp';
